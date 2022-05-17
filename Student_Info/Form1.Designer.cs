08ed3995f7f60bf5e8d88fd23604b1ff85805328
Beer Inder Singh <BeerInder.Singh@techtorium.ac.nz>
2022-05-17 22:25:12 +1200

    Add project files.

---
+++ Student_Info/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Student_Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LB_Student = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Update_Record = new System.Windows.Forms.Button();
+            this.Delete_Record = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LB_Student
+            // 
+            this.LB_Student.FormattingEnabled = true;
+            this.LB_Student.ItemHeight = 16;
+            this.LB_Student.Location = new System.Drawing.Point(708, 15);
+            this.LB_Student.Margin = new System.Windows.Forms.Padding(4);
+            this.LB_Student.Name = "LB_Student";
+            this.LB_Student.Size = new System.Drawing.Size(273, 404);
+            this.LB_Student.TabIndex = 0;
+            this.LB_Student.MouseClick += new System.Windows.Forms.MouseEventHandler(this.LB_Student_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student FName";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(334, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(176, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Student LName";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 182);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mobile Number";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Update_Record
+            // 
+            this.Update_Record.Location = new System.Drawing.Point(348, 251);
+            this.Update_Record.Name = "Update_Record";
+            this.Update_Record.Size = new System.Drawing.Size(162, 38);
+            this.Update_Record.TabIndex = 8;
+            this.Update_Record.Text = "Update";
+            this.Update_Record.UseVisualStyleBackColor = true;
+            this.Update_Record.Click += new System.EventHandler(this.Update_Record_Click);
+            // 
+            // Delete_Record
+            // 
+            this.Delete_Record.Location = new System.Drawing.Point(204, 351);
+            this.Delete_Record.Name = "Delete_Record";
+            this.Delete_Record.Size = new System.Drawing.Size(214, 31);
+            this.Delete_Record.TabIndex = 9;
+            this.Delete_Record.Text = "Delete";
+            this.Delete_Record.UseVisualStyleBackColor = true;
+            this.Delete_Record.Click += new System.EventHandler(this.Delete_Record_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1333, 692);
+            this.Controls.Add(this.Delete_Record);
+            this.Controls.Add(this.Update_Record);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LB_Student);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Student Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LB_Student;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Update_Record;
+        private System.Windows.Forms.Button Delete_Record;
+    }
+}
+
---
+++ Student_Info/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Info
+{
+    public partial class Form1 : Form
+    {
+        public string connectionString = @"Data Source=T2181\SQLEXPRESS; Initial Catalog=student;Integrated Security=True;Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; MultiSubnetFailover=False ";
+        SqlConnection con = new SqlConnection("Data Source=T2181\\SQLEXPRESS; Initial Catalog=student;Integrated Security=True;Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; MultiSubnetFailover=False ");
+        SqlCommand Command = new SqlCommand();
+        SqlDataReader reader;
+        String SqlStmt;
+        int PK = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            //connection string to connect to database
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //SqlConnection con = new SqlConnection(connectionString);
+            //string com = "Select * from Stud_Details";
+            //Command = new SqlCommand(com, con);
+
+            ////Open connection
+            //con.Open();
+
+            ////Start Database reader
+            //reader = Command.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    LB_Student.Items.Add((reader["S_ID"] + " -" + reader["S_Name"] + " " + reader["lastName"] + " (" + reader["Mobile"] + ")"));
+            //}
+
+            ////Close Database reader
+            //reader.Close();
+            ////Close Connection
+            //con.Close();
+            Load_ListBox();
+        }
+
+        public void Load_ListBox()
+        {
+            //SqlConnection con = new SqlConnection(connectionString);
+            SqlStmt = "Select * from Stud_Details";
+            Command = new SqlCommand(SqlStmt, con);
+
+            //Open connection
+            con.Open();
+
+            //Start Database reader
+            reader = Command.ExecuteReader();
+            while (reader.Read())
+            {
+                LB_Student.Items.Add((reader["S_ID"] + " -" + reader["S_Name"] + " " + reader["lastName"] + " (" + reader["Mobile"] + ")"));
+            }
+
+            //Close Database reader
+            reader.Close();
+            //Close Connection
+            con.Close();
+        }
+
+        public void Clear_Fields()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            LB_Student.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+//            Command.Connection = con;
+            Random rnd_num = new Random();
+            int S_ID = rnd_num.Next(1, 10000);
+            string S_Name = textBox1.Text;
+            string lName = textBox2.Text;
+            string Mobile = textBox3.Text;
+            //SqlStmt = "Insert into Stud_Details(S_ID, S_Name, lastName, Mobile) values (@S_ID, @S_Name, @lName, @Mobile)";
+            //using (Command = new SqlCommand(SqlStmt, con))
+            //{
+            //    Command.Parameters.AddWithValue("@S_ID", S_ID);
+            //    Command.Parameters.AddWithValue("@S_Name", S_Name);
+            //    Command.Parameters.AddWithValue("@lastName", lName);
+            //    Command.Parameters.AddWithValue("@Mobile", Mobile);
+            //    con.Open();
+            //    Command.ExecuteNonQuery();
+            //    con.Close();
+            //}
+
+            bool StudentAdded = AddStudent(S_ID, textBox1.Text, textBox2.Text, textBox3.Text);
+            if (StudentAdded)
+            {
+                Clear_Fields();
+                Load_ListBox();
+                
+            }
+        }
+
+        public bool AddStudent(int S_ID, string S_Name, string lastName, string Mobile)
+        {
+            bool studentAdded = false;
+            SqlStmt = "Insert into Stud_Details(S_ID, S_Name, lastName, Mobile) values (@ID, @FName, @LName, @Mobile)";
+            using (Command = new SqlCommand(SqlStmt, con))
+            {
+                Command.Parameters.AddWithValue("@ID", S_ID);
+                Command.Parameters.AddWithValue("@FName", S_Name);
+                Command.Parameters.AddWithValue("@LName", lastName);
+                Command.Parameters.AddWithValue("@Mobile", Mobile);
+                con.Open();
+                int rowsadded = Command.ExecuteNonQuery();
+
+                if (rowsadded > 0)
+                {
+                    studentAdded = true;
+                    MessageBox.Show("Record Added");
+                }
+                con.Close();
+            }
+
+            return studentAdded;
+
+        }
+
+        public bool Load_Data(int S_ID)
+        {
+            SqlStmt = "Select * from Stud_Details where S_ID = " + S_ID;
+            using (Command = new SqlCommand(SqlStmt, con))
+            {
+                con.Open();
+                reader = Command.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        textBox1.Text = reader["S_Name"].ToString();
+                        textBox2.Text = reader["lastName"].ToString();
+                        textBox3.Text = reader["Mobile"].ToString();
+                    }
+                        
+                }
+                con.Close();
+            }
+            return true;
+        }
+
+        private void LB_Student_MouseClick(object sender, MouseEventArgs e)
+        {
+            var selectedValue = LB_Student.SelectedItem;
+            if(selectedValue != null)
+            {
+                MessageBox.Show(selectedValue.ToString());
+            }
+            string fetchedData = LB_Student.SelectedItem.ToString();
+            string[] words = fetchedData.Split(' ');
+            PK = Int16.Parse( words[0]);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            Load_Data(PK);
+        }
+
+        private void Update_Record_Click(object sender, EventArgs e)
+        {
+            string S_Name = textBox1.Text;
+            string lName = textBox2.Text;
+            string Mobile = textBox3.Text;
+
+            bool record_updated = UpdateStudent(textBox1.Text, textBox2.Text, textBox3.Text);
+        }
+
+        public bool UpdateStudent(string S_Name, string lastName, string Mobile)
+        {
+            bool record_Updated = false;
+            SqlStmt = "Update Stud_Details set S_Name = @FName, lastName = @LName, Mobile = @Mobile where S_ID = " + PK;
+
+            using (Command = new SqlCommand(SqlStmt, con))
+            {
+                //                Command.Parameters.AddWithValue("@ID", S_ID);
+                Command.Parameters.AddWithValue("@FName", S_Name);
+                Command.Parameters.AddWithValue("@LName", lastName);
+                Command.Parameters.AddWithValue("@Mobile", Mobile);
+                Command.CommandText = SqlStmt;
+                con.Open();
+                int dataUpdated = Command.ExecuteNonQuery();
+                if(dataUpdated > 0)
+                {
+                    record_Updated = true;
+                    con.Close();
+                    Clear_Fields();
+                    Load_ListBox();
+                }
+                
+            }
+
+                return record_Updated;
+        }
+
+        private void Delete_Record_Click(object sender, EventArgs e)
+        {
+            SqlStmt = "Delete from Stud_Details where S_ID = " + PK;
+            using (Command = new SqlCommand(SqlStmt, con))
+            {
+                con.Open();
+                Command.ExecuteNonQuery();
+                con.Close();
+                Clear_Fields();
+                Load_ListBox();
+                
+            }
+        }
+            
+    }
+}
